8d0468901a7576438becb12425c5ab1e710b5b6b
Ekaterina Machneva <katya@Ekaterinas-MacBook-Air.local>
2022-01-25 09:23:36 -0800

    productsBaby.js

---
+++ JavaScript/productsBaby.js
@@ -0,0 +1,88 @@
+// Get initial baby girls
+const babyGirls = document.getElementById('babyGirls');
+
+getBabyGirls("http://localhost:8080/products/type/BABYGIRL")
+
+//FETCHING PRODUCTS BABY GIRLS BY URL
+async function getBabyGirls(url){
+  const res = await fetch(url, {mode: 'cors'});
+  const data = await res.json();
+
+  showBabyGirls(data);
+}
+
+function showBabyGirls(data){
+
+    babyGirls.innerHTML = '';
+
+  data.forEach((babyGirl) => {
+    // destructuring
+   const { img, name, size, price } = babyGirl
+
+    //constructing div with real product data and then putting it into DOM
+    const babyGirlElement = document.createElement('div')
+    babyGirlElement.classList.add('babyGirls')
+
+    babyGirlElement.innerHTML = `
+
+    <div class=" col card" style="width: 18rem;">
+                    <img src= "${babyGirl.img}" class="card-img-top" alt="Girl's top">
+                    <div class="card-body">
+                      <h5 class="card-title">$${babyGirl.price}</h5>
+                      <p class="card-text">${babyGirl.name} </p>
+                      <a href="#" class="btn btn-primary">ADD TO BAG</a>
+                    </div>
+   `
+
+   babyGirls.appendChild(babyGirlElement)
+
+  })
+}
+
+
+
+
+
+
+
+
+// Get initial baby boys
+const babyBoys = document.getElementById('babyBoys');
+
+getBabyBoys("http://localhost:8080/products/type/BABYBOY")
+
+//FETCHING PRODUCTS BABY BOYS BY URL
+async function getBabyBoys(url){
+  const res = await fetch(url, {mode: 'cors'});
+  const data = await res.json();
+
+  showBabyBoys(data);
+}
+
+function showBabyBoys(data){
+
+    babyBoys.innerHTML = '';
+
+  data.forEach((babyBoy) => {
+    // destructuring
+   const { img, name, size, price } = babyBoy
+
+    //constructing div with real product data and then putting it into DOM
+    const babyBoyElement = document.createElement('div')
+    babyBoyElement.classList.add('babyBoys')
+
+    babyBoyElement.innerHTML = `
+
+    <div class=" col card" style="width: 18rem;">
+                    <img src= "${babyBoy.img}" class="card-img-top" alt="Girl's top">
+                    <div class="card-body">
+                      <h5 class="card-title">$${babyBoy.price}</h5>
+                      <p class="card-text">${babyBoy.name} </p>
+                      <a href="#" class="btn btn-primary">ADD TO BAG</a>
+                    </div>
+   `
+
+   babyBoys.appendChild(babyBoyElement)
+
+  })
+}
